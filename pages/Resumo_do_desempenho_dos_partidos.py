daeafc3c58883444599adebd14740c3da7d5b6ad
Ricardo S. P. Lopes <77239923+ojoS2@users.noreply.github.com>
2024-12-03 07:09:03 -0300

    Update Resumo_do_desempenho_dos_partidos.py

--- pages/Resumo_do_desempenho_dos_partidos.py
+++ pages/Resumo_do_desempenho_dos_partidos.py
@@ -3,7 +3,6 @@ import json
 import pandas as pd
 from dash import Dash, html, dcc, Input, Output, State, callback, Patch
 import dash_bootstrap_components as dbc
-import dash_ag_grid as dag
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
