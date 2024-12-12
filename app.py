d4587944d7e32fab9dae2042b0954d0114259347
Ricardo <ricardo@trycorder2.0>
2024-12-12 06:37:23 -0300

    update req

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import dash
-import json
-import pandas as pd
 from dash import Dash, html, dcc, Input, Output, State, callback, Patch
 import dash_bootstrap_components as dbc
-import dash_ag_grid as dag
-import plotly.graph_objects as go
-import plotly.express as px
 
 #app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP],
